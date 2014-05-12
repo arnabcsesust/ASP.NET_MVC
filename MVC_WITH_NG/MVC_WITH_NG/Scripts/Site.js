2c8098ea38daa3772c575ed3af23caaadb99a88c
arnabcsesust <arnabcsesust@gmail.com>
2014-05-13 00:29:48 +0600

    configure angular with asp.net mvc

--- MVC_WITH_NG/MVC_WITH_NG/Scripts/Site.js
+++ MVC_WITH_NG/MVC_WITH_NG/Scripts/Site.js
@@ -1,1 +1,14 @@
-﻿
+﻿var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '../Ng_Templates/home.html',
+        controller: 'HomeController'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
+
+app.controller('HomeController', function () {
+
+});
