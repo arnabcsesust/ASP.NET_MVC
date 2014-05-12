14aee90518412e985ab53e9c06af483fe41fdba8
arnabcsesust <arnabcsesust@gmail.com>
2014-05-13 01:08:55 +0600

    understanding $scope

--- MVC_WITH_NG/MVC_WITH_NG/Scripts/Site.js
+++ MVC_WITH_NG/MVC_WITH_NG/Scripts/Site.js
@@ -1,14 +1,26 @@
 ﻿var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: '../Ng_Templates/home.html',
-        controller: 'HomeController'
-    }).otherwise({
-        redirectTo: '/'
-    });
+    $routeProvider
+        .when('/', {
+            templateUrl: '../Ng_Templates/home.html',
+            controller: 'HomeController'
+        })
+        .when('/settings', {
+            templateUrl: '../Ng_Templates/settings.html',
+            controller: 'SettingsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
 
-app.controller('HomeController', function () {
+app.controller('HomeController', function ($scope) {
 
 });
+
+app.controller('SettingsController', function ($scope) {
+    $scope.person = {
+        name: 'Arnab'
+    };
+}); 
